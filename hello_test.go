37fba48b10feab66f5bceac52d9cd0b48874ba1d
Simone D'Angelo <sim.dangelo.95@gmail.com>
2025-11-02 23:49:56 +0100

    refactor test to avoid code duplication

--- hello_test.go
+++ hello_test.go
@@ -6,17 +6,22 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// t.Helper() is needed to tell the test suite that this method is a helper.
+	// By doing this, when it fails, the line number reported will be in our function call rather than inside our test helper. 
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
