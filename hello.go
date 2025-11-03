106c66cf309bd7c2334fcead42e1461816e9c436
Simone D'Angelo <sim.dangelo.95@gmail.com>
2025-11-03 23:05:52 +0100

    add a version2 of already existing function

--- hello.go
+++ hello.go
@@ -22,6 +22,19 @@ func greetingPrefix(language string) (prefix string) {
 	}
 	return
 }
+// Version 2
+// func greetingPrefix(language string) string {
+// 	var prefix string
+// 	switch language {
+// 	case spanish:
+// 		prefix = spanishHelloPrefix
+// 	case french:
+// 		prefix = frenchHelloPrefix
+// 	default:
+// 		prefix = englishHelloPrefix
+// 	}
+// 	return prefix
+// }
 
 func Hello(name string, language string) string {
 	if name == "" {
