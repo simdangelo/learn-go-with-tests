56b76b1de2eb2068f3f36e70fc3ec1c0fb47b2b5
Simone D'Angelo <sim.dangelo.95@gmail.com>
2025-11-03 23:03:55 +0100

    little refactor

--- hello.go
+++ hello.go
@@ -2,23 +2,32 @@ package main
 
 import "fmt"
 
-const spanish = "Spanish"
-const french = "French"
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+const (
+	spanish = "Spanish"
+	french = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix = "Bonjour, "
+)
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
-	}
-	if language == spanish {
-		return spanishHelloPrefix + name
-	}
-	if language == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+	}	
+	return greetingPrefix(language) + name
 }
 
 func main() {
